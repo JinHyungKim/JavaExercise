a45048f45c7397d4188127f06fa6dc15d3174eed
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-05-28 22:20:28 +0900

    Create javaExercise27_1_2.java

---
+++ javaExercise27_1_2.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
+// class SLenComp implements Comparator<String>{
+// 	@Override
+// 	public int compare(String s1, String s2){
+// 		return s1.length() - s2.length();
+// 	}
+// }
+
+class javaExercise27_1_2{
+	public static void main(String[] args){
+		List<String> list = new ArrayList<>();
+		list.add("Robot");
+		list.add("Lambda");
+		list.add("Box");
+
+//		SLenComp cmp = new SLenComp();
+		Collections.sort(list, (s1, s2)->{return s1.length() - s2.length();});
+		System.out.println(list); 
+	}
+}
