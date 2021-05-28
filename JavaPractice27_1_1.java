d739f0c56296dba216ba9f376dbe917a43ebe3e7
JinHyungKim <83858427+JinHyungKim@users.noreply.github.com>
2021-05-28 22:15:08 +0900

    Create JavaPractice27_1_1.java

---
+++ JavaPractice27_1_1.java
@@ -0,0 +1,17 @@
+interface Calculate<T>{
+	T cal(T a, T b);
+}
+
+class JavaPractice27_1_1{
+	public static <T> void calAndShow(Calculate<T> op, T n1, T n2){
+		T r = op.cal(n1, n2);
+		System.out.println(r);
+	}
+
+	public static void main(String[] args){
+		calAndShow((a,b)->{return a+b;}, 3, 4);
+		calAndShow((a,b)->{return a+b;}, 2.5, 7.1);
+		calAndShow((a,b)->{return a-b;}, 4, 2);
+		calAndShow((a,b)->{return a+b;}, 4.9, 3.2);
+	}
+}
